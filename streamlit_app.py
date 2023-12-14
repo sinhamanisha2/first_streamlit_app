e3dccec394ac456fb08250dbb8bb14c3ee5bf264
Manisha Sinha <sinhamanisha182@gmail.com>
2023-12-14 10:43:35 +0530

    Update streamlit_app.py
    adding content in the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Blueberry Oatmeal')
+streamlit.text('Spinach and Rocket Smoothie')
+streamlit.text('Hard-boiled egg')
