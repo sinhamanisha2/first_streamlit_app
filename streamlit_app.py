b46d35c9a7a34ba52a5892ba79ff89609f1ed2df
Manisha Sinha <sinhamanisha182@gmail.com>
2023-12-14 10:53:18 +0530

    Update streamlit_app.py
    using panda to display the fruit list from bucket

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('🥣Breakfast Menu')
@@ -8,3 +8,5 @@ streamlit.text('🥑🍞Avacado Toast')
 streamlit.text('🐔Hard-boiled egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
